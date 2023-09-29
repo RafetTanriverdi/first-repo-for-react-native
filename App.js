f389c427230673c486f4eaec6eaa39574745b85b
RafetTanriverdi <by.rafet.taniverdi@gmail.com>
2023-09-29 22:46:10 +0300

    flex-box-test

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.innerText}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.upper_container}>
+        <Text style={styles.innerText}>
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
+
+      <View style={styles.bottom_container}>
+        <Text style={styles.innerText}>
+          Open up App.js to start working on your app$
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection:"row"
   },
-  innerText: {
-    color: "white",
+  upper_container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  bottom_container: {
+    flex: 1,
+    backgroundColor: "blue",
   },
 });
